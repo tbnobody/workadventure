80315bd85ce6c7bbd9017b94ed3c662771abc51f
Piotr Dobrowolski <admin@tastycode.pl>
2021-01-03 12:23:13 +0100

    front: add AnimatedTiles plugin

--- front/src/Phaser/Game/GameScene.ts
+++ front/src/Phaser/Game/GameScene.ts
@@ -92,6 +92,8 @@ import {joystickBaseImg, joystickBaseKey, joystickThumbImg, joystickThumbKey} fr
 import { InteractiveLayer } from "../Map/InteractiveLayer";
 import {videoManager} from "../../WebRtc/VideoManager";
 
+import AnimatedTiles from "phaser-animated-tiles";
+
 export interface GameSceneInitInterface {
     initPosition: PointInterface|null,
     reconnecting: boolean
@@ -139,6 +141,7 @@ export class GameScene extends ResizableScene implements CenterListener {
     interactiveLayers!: Array<InteractiveLayer>;
     Objects!: Array<Phaser.Physics.Arcade.Sprite>;
     mapFile!: ITiledMap;
+    animatedTiles!: AnimatedTiles;
     groups: Map<number, Sprite>;
     startX!: number;
     startY!: number;
@@ -255,6 +258,7 @@ export class GameScene extends ResizableScene implements CenterListener {
                 message: this.originalMapUrl ?? file.src
             });
         });
+        this.load.scenePlugin('AnimatedTiles', AnimatedTiles, 'animatedTiles', 'animatedTiles');
         this.load.on('filecomplete-tilemapJSON-'+this.MapUrlFile, (key: string, type: string, data: unknown) => {
             this.onMapLoad(data);
         });
@@ -456,6 +460,7 @@ export class GameScene extends ResizableScene implements CenterListener {
 
         this.initCamera();
 
+        this.animatedTiles.init(this.Map);
         this.initCirclesCanvas();
 
         // Let's pause the scene if the connection is not established yet
